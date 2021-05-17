b30417e7ecbbd6987b6cf0b916fa2dbdd0ad362a
miki <miki@LAPTOP-JQSF7IN9.localdomain>
2021-05-17 17:30:52 +0200

    add html file to display results

--- rest-client/src/main/java/assignment/restclient/controllers/QuoteController.java
+++ rest-client/src/main/java/assignment/restclient/controllers/QuoteController.java
@@ -1,20 +1,12 @@
 package assignment.restclient.controllers;
 
-import assignment.restclient.dtos.Quote;
-import assignment.restclient.dtos.QuoteScore;
-import assignment.restclient.services.GetQuoteService;
 import assignment.restclient.services.QuotesWithScoresService;
-import assignment.restclient.services.ValidateQuoteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-@RestController
+@Controller
 @RequestMapping("/")
 public class QuoteController
 {
@@ -22,7 +14,9 @@ public class QuoteController
     private QuotesWithScoresService quotesWithScoresService;
 
     @GetMapping("{count}")
-    public Map<String,Double> getScore(@PathVariable int count){
-        return quotesWithScoresService.getResults(count);
+    public String getScore(@PathVariable int count, Model model){
+        model.addAttribute("quotes", quotesWithScoresService.getResults(count));
+        // Map<String, Double> res = quotesWithScoresService.getResults(count);
+        return "results";
     }
 }
